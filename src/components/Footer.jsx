5422e25c2e9c7f3e8d57badc271bb9f85621b38c
William Alderette <williamalderette799@gmail.com>
2024-12-17 02:17:48 -0600

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,11 @@
-const Footer = () => {
-    return (
-        <div></div>
-    );
-}
+import React from 'react';
 
+const Footer = () => {
+  return <footer className='bg-black py-12 '>
+    <div className='container max-auto'>
+      <p className='text-white text-center'>Copyright &copy; Ecommerce Shop 2022. All rights reserved.</p>
+    </div>
+  </footer>;
+};
 
 export default Footer;
